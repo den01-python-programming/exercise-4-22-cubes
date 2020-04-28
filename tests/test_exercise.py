81645bb76d9ad87db65e8e36c44e65e1d16c008d
Scott Morgan <smorgan@bridgend.ac.uk>
2020-04-28 15:55:48 +0100

    Updates testing routines

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -2,5 +2,20 @@ import pytest
 import src.exercise
 
 def test_exercise():
-    #implement tests here
-    assert 0 == 0
+    input_values = ["3","2","-3","end"]
+    output = []
+
+    def mock_input(s=None):
+        if s is not None:
+            output.append(s)
+            return input_values.pop(0)
+        else:
+            output.append("")
+            return input_values.pop(0)
+
+    src.exercise.input = mock_input
+    src.exercise.print = lambda s : output.append(s)
+
+    src.exercise.main()
+
+    assert output == ["",27,"",8,"",-27,""]
